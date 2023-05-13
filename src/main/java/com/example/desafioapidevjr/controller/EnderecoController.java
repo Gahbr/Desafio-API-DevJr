a613627946a1c33f1603f7bced9860e12f4a0c4a
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2023-05-13 15:00:48 -0300

    :recycle: refactor: Codigo HTTP correto

--- src/main/java/com/example/desafioapidevjr/controller/EnderecoController.java
+++ src/main/java/com/example/desafioapidevjr/controller/EnderecoController.java
@@ -3,6 +3,7 @@ package com.example.desafioapidevjr.controller;
 import com.example.desafioapidevjr.dto.EnderecoDTO;
 import com.example.desafioapidevjr.entities.Endereco;
 import com.example.desafioapidevjr.service.EnderecoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +19,7 @@ public class EnderecoController {
     public ResponseEntity<Endereco> newAddress(@RequestBody EnderecoDTO request) {
         try {
             var response = enderecoService.createAddress(request);
-            return ResponseEntity.ok().body(response);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
         }catch (Exception error){
             return ResponseEntity.badRequest().header("Error-Message", error.getMessage()).build();
         }
--- src/main/java/com/example/desafioapidevjr/service/IEnderecoService.java
+++ src/main/java/com/example/desafioapidevjr/service/IEnderecoService.java
@@ -2,7 +2,6 @@ package com.example.desafioapidevjr.service;
 
 import com.example.desafioapidevjr.dto.EnderecoDTO;
 import com.example.desafioapidevjr.entities.Endereco;
-import com.example.desafioapidevjr.service.viaCep.ViaCepResponse;
 
 public interface IEnderecoService {
     Endereco createAddress(EnderecoDTO request) throws Exception;
